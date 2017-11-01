92b0ed8b8b6c95896bc27c1a496ae7a9569c52af
yotam18-meet <yotamasael7@gmail.com>
2017-11-01 19:26:08 +0200

    nessa

---
+++ lab3/turtleshape.py
@@ -0,0 +1,12 @@
+import turtle
+def ex1():
+	amont=5
+	forw=100
+	turn=144
+	color = ['red','blue','green','black,'yellow']
+	for i in range (amount)
+def ex2():
+	turtle.forward(50)
+	turtle.right(270)
+	turtle.forward(50)
+turtle.mainloop()
