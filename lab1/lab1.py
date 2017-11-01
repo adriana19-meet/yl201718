03265a2e44de5907d97ca625b0c1d5b0a2f71808
yotam18-meet <yotamasael7@gmail.com>
2017-11-01 18:54:38 +0200

    nessa

---
+++ lab1/lab1.py
@@ -0,0 +1,27 @@
+print("adriana")*100
+number1=10
+print(number1)
+number2 = number1/2
+print(number2)
+lst = [5,10,2001]
+print(lst[0]*2)
+print(lst[1]*2)
+print(lst[2]*2)
+import turtle
+turtle.begin_fill()
+turtle.goto(0,100)
+turtle.goto(100,100)
+turtle.goto(100,0)
+turtle.goto(0,0)
+turtle.end_fill()
+turtle.circle(165)
+turtle.mainloop()
+import turtle 
+turtle.penup()
+turtle.begin_fill()
+turtle.goto(100,100)
+turtle.pendown()
+turtle.goto(200,100)
+turtle.goto(200,0)
+turtle.goto(100.0)
+turtle.goto(100,100)
---
+++ lab2/lab2.py
@@ -0,0 +1,35 @@
+a=[5,10,15,20,25]
+b=[]
+def list(a):
+	b=[a[0],a[-1]] 
+	return b 
+print(list(a))
+
+
+
+y=[1,1,2,3,5,8,13,21,34,55,89]
+for i in range(len(y)):
+	if y[i]<5:
+		print(y[i])
+
+
+
+
+#define a function that takes two arguments(list1,list2)
+#make a for loop that goes throw the whole list; for value in list1:
+#we need to check if the value is in list2; if value in list2:
+#print(value)
+
+y=[1,1,2,3,5,8,13,21,34,55,89]
+j=[1,2,3,4,5,6,7,8,9,,10,11,12,13]
+def function(y,j):
+	for i in range(len(y))
+	
+
+ 
+
+
+
+
+
+
